9c1fdc8c59a6dba91ce424cd80dcceaed5c9da9f
AvielDagan <48187820+AvielDagan@users.noreply.github.com>
2020-06-17 12:55:43 +0300

    styling

--- src/routes/Approval/Approval.js
+++ src/routes/Approval/Approval.js
@@ -18,47 +18,36 @@ class Approval extends Component {
     }
 
     render() {
-        return ( <
-            div className = "Approval" >
-            <
-            div className = "Approval-header" >
-            <
-            AppBar className = "Approval-text"
-            position = "static" >
-            <
-            Typography variant = 'h6' > Almost Complete < /Typography> < /
-            AppBar > <
-            /div> <
-            div className = "Approval-content" >
-            <
-            div className = "Approval-Title" >
-            <
-            p > We are ONE step away! < /p> < /
-            div > <
-            div className = "Approval-centerContent" >
-            <
-            p >
-            Hey username, < br / >
-            Now you just need to wait
-            for your product gets to you, < br / >
-            that great news!
-            <
-            /p> < /
-            div > <
-            div className = "Approval-bottomContent" >
-            <
-            p >
-            We are glad to help you < br / >
-            LoBassa Team. <
-            /p> < /
-            div > <
-            div className = "Approval-logo" >
-            <
-            Logo / >
-            <
-            /div> < /
-            div > <
-            /div>
+        return (
+            <div className="Approval">
+                <div className="Approval-header">
+                    <AppBar className="Approval-text" position="static" >
+                        <Typography variant='h6' > Almost Complete </Typography>
+                    </AppBar >
+                </div>
+                <div className="Approval-content" >
+                    <div className="Approval-Title" >
+                        <p> We are ONE step away! </p>
+                    </div>
+                    <div className="Approval-centerContent" >
+                        <p>
+                            Hey username, < br />
+                            Now you just need to wait
+                            for your product gets to you, <br />
+                            that great news!
+                        </p>
+                    </div>
+                    <div className="Approval-bottomContent" >
+                        <p>
+                            We are glad to help you < br />
+                            LoBassa Team.
+                        </p>
+                    </div >
+                    <div className="Approval-logo" >
+                        <Logo />
+                    </div>
+                </div >
+            </div>
         )
     }
 }
