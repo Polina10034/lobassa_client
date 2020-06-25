b65a69f6056129644fe5e1b33fbd563297096cfb
poli10034 <poli10034@gmail.com>
2020-06-25 12:40:47 +0300

    admin dash

--- src/routes/adminDash/AdminDash.js
+++ src/routes/adminDash/AdminDash.js
@@ -10,34 +10,9 @@ function AdminDash() {
   const session = useSelector(state => state.session)
   console.log('Current Session: ', session)
 
-<<<<<<< HEAD
-  const useMountEffect = (fun) => useEffect(fun, [])
-
-  useMountEffect(function () {
-    axios.get('https://api.lobassa.com/statistics')
-      .then(res => { setData(res.data.body); setLoading(false) })
-  })
-
-  function isLoading () {
-    return (
-      <div>
-        <span>loading...</span>
-      </div>
-    )
-  }
-
-  function adminPage (data) {
-    return (
-      <div className="App">
-        <Dashboard data={data}></Dashboard>
-      </div>
-    )
-  }
-=======
   // if (!session.isLoggedIn) {
   //   return <Redirect to="/" />
   // }
->>>>>>> 9c93aed73b73f7cdcb004a38af858501bf61def5
 
   return (
     <div className="App">
